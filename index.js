127afba35d0a0a5b30d4569041c7880793c94504
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-01-05 23:05:45 +0900

    機能追加
    カテゴリで商品絞り込み

--- index.js
+++ index.js
@@ -21,8 +21,14 @@ app.use(methodOverride('_method'));
 
 const categories = ['果物', '野菜', '乳製品', 'パン類', 'その他'];
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products });
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: '全' });
+    }
 });
 
 app.get('/products/new', (req, res) => {
